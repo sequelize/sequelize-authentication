49c83445f1a989e5b6633765b974c6273aff230f
Sascha Depold <sascha@depold.com>
2013-02-15 21:46:19 +0100

    support for header and post body

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -7,31 +7,47 @@ var buster         = require('buster')
 buster.spec.expose()
 
 describe('sequelize-authentication', function() {
-  describe('authentication data in query', function() {
-    before(function() {
-      this.authenticate = Authentication(sequelize, { credentialSource: 'query' })
-    })
+  var tests = [{
+    title:         'authentication data in query',
+    via:           'query',
+    dataAttribute: 'query'
+  }, {
+    title:         'authentication data in post body',
+    via:           'body',
+    dataAttribute: 'body'
+  }, {
+    title:         'authentication data in headers',
+    via:           'headers',
+    dataAttribute: 'headers'
+  }]
 
-    it('raises an error if authentication fails', function(done) {
-      this.authenticate({
-        query:   { user: uuid.v1(), password: uuid.v1() },
-        headers: {}
-      }, {
-        end: function(msg) {
-          expect(msg).toEqual('Unauthorized')
+  tests.forEach(function(test) {
+    describe(test.title, function() {
+      before(function() {
+        this.authenticate = Authentication(sequelize, { via: test.via })
+      })
+
+      it('raises an error if authentication fails', function(done) {
+        var options = { headers: {} }
+        options[test.dataAttribute] = { user: uuid.v1(), password: uuid.v1() }
+
+        this.authenticate(options, {
+          end: function(msg) {
+            expect(msg).toEqual('Unauthorized')
+            done()
+          }
+        }, function(){})
+      })
+
+      it('calls the passed function if credentials are correct', function(done) {
+        var options = { headers: {} }
+        options[test.dataAttribute] = { user: 'root', password: '' }
+
+        this.authenticate(options, {}, function(){
+          expect(1).toEqual(1)
           done()
-        }
-      }, function(){})
-    })
-
-    it('calls the passed function if credentials are correct', function(done) {
-      this.authenticate({
-        query:   { user: 'root', password: '' },
-        headers: {}
-      }, {}, function(){
-        expect(1).toEqual(1)
-        done()
+        })
       })
     })
-  })
+  }.bind(this))
 })
