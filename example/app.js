ca47addc062a831967881e02e6d7a4e1bb0ce2f4
Sascha Depold <sascha@depold.com>
2013-02-17 14:14:42 +0100

    example application

---
+++ example/app.js
@@ -0,0 +1,33 @@
+var express      = require('express')
+  , http         = require('http')
+  , authenticate = require(__dirname + '/../lib/index')
+  , Sequelize    = require('sequelize')
+  , sequelize    = new Sequelize('sequelize_authentication', 'root', null, { logging: false })
+  , app          = express()
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000)
+
+  app.use(express.favicon())
+  app.use(express.bodyParser())
+  app.use(express.methodOverride())
+
+  // app.use(authenticate(sequelize, { via: 'query' }))
+  // app.use(authenticate(sequelize, { via: 'body' }))
+  // app.use(authenticate(sequelize, { via: 'params' }))
+  app.use(authenticate(sequelize, { via: 'headers' }))
+  app.use(app.router)
+})
+
+app.get('/', function(req, res, next) {
+  res.send('hello world')
+})
+
+app.post('/', function(req, res, next) {
+  res.send('hello world')
+})
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'))
+})
